a856f23d004a21bdcd56489483646368997161b5
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-20 10:03:19 +0600

    Connected done with mongoDB

--- index.js
+++ index.js
@@ -12,6 +12,38 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhkv2dd.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('Database Connection Successful')
+
+        // Connecting to cluster
+        const appointmentOptions = client.db('doctors_portal').collection('appointment_options');
+
+        // Creating api
+        app.get('/appointment_options', async (req, res) => {
+            const query = {};
+            const cursor = appointmentOptions.find(query);
+            const options = await cursor.toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Doctors Portal')
 })
