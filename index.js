4e9734676426bdc6abda51ea2903eec7f75dabbb
Md. Nafish Sadik <md.nafish.sadik@gmail.com>
2023-11-20 09:25:24 +0600

    Necessary installation done and basic settings added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const app = express()
 const port = process.env.PORT || 5000;
-// helloworld
+
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhkv2dd.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get('/', (req, res) => {
     res.send('Doctors Portal')
